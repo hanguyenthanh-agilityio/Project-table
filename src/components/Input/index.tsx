38b752d6fb8347146e698b1bf892647013804bef
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-09-20 08:29:20 +0700

    feat: implement input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,58 @@
+import { memo } from "react";
+
+// Chakra UI
+import {
+  Icon,
+  Input as SelectChakra,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
+interface SearchInputProps {
+  placeholder?: string;
+  onChange?: () => void;
+  onKeyDown: () => void;
+  value?: string;
+}
+
+const Input = memo<SearchInputProps>(
+  ({
+    placeholder = "Search",
+    onChange,
+    onKeyDown,
+    value,
+  }: SearchInputProps) => {
+    return (
+      <InputGroup maxW={{ xs: "100%", lg: "300px" }}>
+        <SelectChakra
+          id="myInput"
+          size={{ xs: "secondary", lg: "default" }}
+          fontSize={{ xs: "12px", md: "17px" }}
+          pl="40px"
+          name="input"
+          data-testid="input-field"
+          placeholder={placeholder}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={value}
+        />
+        <InputRightElement
+          right={{ xs: "10px", md: "16px" }}
+          bottom="0px"
+          top="0px"
+          children={
+            <Icon
+              as={SearchIcon}
+              color="text.default"
+              w={{ xs: "15px", md: "22px" }}
+              h={{ xs: "15px", md: "22px" }}
+            />
+          }
+        />
+      </InputGroup>
+    );
+  },
+);
+
+export default Input;
