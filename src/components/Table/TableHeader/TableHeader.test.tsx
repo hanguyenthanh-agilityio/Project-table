1ea4a5219f9fd74af218d10de628e69ed4d359b5
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-09-23 16:30:24 +0700

    feat: update unit test

--- src/components/Table/TableHeader/TableHeader.test.tsx
+++ src/components/Table/TableHeader/TableHeader.test.tsx
@@ -6,12 +6,13 @@ import { Table } from "@chakra-ui/react";
 
 // Constants
 import { HEADER_TABLE } from "@/constants/table";
+import { PROJECT_LIST } from "@/mocks/table";
 
 describe("Select component", () => {
   it("should render TableHeader snapshot correctly", () => {
     const Header = render(
       <Table>
-        <TableHeader headerList={HEADER_TABLE} />
+        <TableHeader headerList={HEADER_TABLE} projects={PROJECT_LIST} />
       </Table>,
     );
     expect(Header).toMatchSnapshot();
